431c2452cb33c9dbf505bfa9dbd4969237a5fa20
Rich <richard.wu@ontariotechu.net>
2025-02-13 14:12:45 -0500

    Added bubble sort

---
+++ BubbleSort.java
@@ -0,0 +1,33 @@
+public class BubbleSort {
+
+    // Method to implement Bubble Sort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+
+        // Traverse through all array elements
+        for (int i = 0; i < n - 1; i++) {
+            // Last i elements are already sorted, so we can reduce the range
+            for (int j = 0; j < n - i - 1; j++) {
+                // Swap if the element found is greater than the next element
+                if (arr[j] > arr[j + 1]) {
+                    // Swap temp and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Utility method to print the array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
